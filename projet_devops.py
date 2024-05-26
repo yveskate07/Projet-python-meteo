3f64719c402c4119ff1bf59bf6eeb95c8302d12c
yveskate07 <118679761+yveskate07@users.noreply.github.com>
2024-05-26 15:01:58 +0000

    Update projet_devops.py

--- projet_devops.py
+++ projet_devops.py
@@ -1,9 +1,8 @@
-import requests
-import pprint
+import requests # pour lancer les requêtes HTTP
+import pprint # pour formater la reponse obtenue en format json
 print ("Bienvenu sur l'application le site qui vous offre les  informations meteologique du monde entier")
-API_Key = '0f70f1694a9a05acfae04d35479d6b49'
-city = input("veiilez saisir votre ville: ")
-base_url =f"https://api.openweathermap.org/data/2.5/weather?appid={API_Key}&q={city}"
-weather_data=requests.get(base_url).json()
-print(weather_data)
-
+API_Key = '0f70f1694a9a05acfae04d35479d6b49' # clé API récupérée sur le site openweathermap.org
+city = input("veiilez saisir votre ville: ") # le nom de la ville dont on souhaiterais obtenir les informations météorologiques
+base_url =f"https://api.openweathermap.org/data/2.5/weather?appid={API_Key}&q={city}" # l'url vers la page qui contient les informations recherchées
+weather_data=requests.get(base_url).json() # lancement de la requête HTTP vers l'url ci-dessus
+print(weather_data) # affichage de la reponse obtenue sous format JSON
